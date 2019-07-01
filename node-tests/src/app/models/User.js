61edbb01bf565ce0832cec34508f051db9812ed9
wagner.dutra <wagnerdutra1010@gmail.com>
2019-07-01 11:12:31 -0300

    Removing project

--- node-tests/src/app/models/User.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password_hash: DataTypes.STRING
-  });
-
-  return User;
-};
